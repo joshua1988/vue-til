4fcbe7cb49c527417b81d599e168a838606941f6
joshua1988 <jangkeehyo@gmail.com>
2020-01-05 13:00:56 +0900

    complete router

---
+++ src/routes/index.js
@@ -0,0 +1,28 @@
+import Vue from 'vue';
+import VueRouter from 'vue-router';
+// import LoginPage from '@/views/LoginPage.vue';
+// import SignupPage from '@/views/SignupPage.vue';
+
+Vue.use(VueRouter);
+
+export default new VueRouter({
+	mode: 'history',
+	routes: [
+		{
+			path: '/',
+			redirect: '/login',
+		},
+		{
+			path: '/login',
+			component: () => import('@/views/LoginPage.vue'),
+		},
+		{
+			path: '/signup',
+			component: () => import('@/views/SignupPage.vue'),
+		},
+		{
+			path: '*',
+			component: () => import('@/views/NotFoundPage.vue'),
+		},
+	],
+});
