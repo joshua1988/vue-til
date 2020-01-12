00ad20ca58b4ce2034e202297ef9f4a2dd0c762b
joshua1988 <jangkeehyo@gmail.com>
2020-01-12 15:24:24 +0900

    add font, icon cdns and polish the css styles

--- src/components/common/AppHeader.vue
+++ src/components/common/AppHeader.vue
@@ -3,13 +3,15 @@
     <div>
       <router-link to="/" class="logo">
         TIL
+        <span v-if="isUserLogin">by {{ $store.state.username }}</span>
       </router-link>
     </div>
     <div class="navigations">
       <!-- 1 -->
       <template v-if="isUserLogin">
-        <span class="username">{{ $store.state.username }}</span>
-        <a href="javascript:;" @click="logoutUser">Logout</a>
+        <a href="javascript:;" @click="logoutUser" class="logout-button">
+          Logout
+        </a>
       </template>
       <!-- 2 -->
       <template v-else>
@@ -69,6 +71,9 @@ a.logo {
   position: fixed;
   top: 0;
   width: 100%;
+}
+.logout-button {
+  font-size: 14px;
 }
 a.router-link-exact-active {
   color: white;
--- src/views/MainPage.vue
+++ src/views/MainPage.vue
@@ -1,6 +1,8 @@
 <template>
   <div>
-    <h1>Main</h1>
+    <div class="main list-container contents">
+      <h1 class="page-header">Today I Learned</h1>
+    </div>
   </div>
 </template>
 
