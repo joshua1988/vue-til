68bd570badc94fc8a165777cf5b448289a7b6ca8
joshua1988 <jangkeehyo@gmail.com>
2020-01-12 17:21:34 +0900

    add loading spinner

---
+++ src/components/common/LoadingSpinner.vue
@@ -0,0 +1,35 @@
+<template>
+  <div class="spinner-container">
+    <div class="spinner" />
+  </div>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style scoped>
+.spinner-container {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  height: 240px;
+}
+.spinner {
+  width: 50px;
+  height: 50px;
+  border-radius: 50%;
+  border: 5px solid #e0e0e0;
+  border-bottom: 5px solid #fe9616;
+  animation: spin 1s linear infinite;
+  position: relative;
+}
+@keyframes spin {
+  from {
+    transform: rotate(0deg);
+  }
+  to {
+    transform: rotate(360deg);
+  }
+}
+</style>
