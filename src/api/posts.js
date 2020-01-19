9b4a2ec6839839efa27494df7cdc8c330d83fa53
joshua1988 <jangkeehyo@gmail.com>
2020-01-19 20:59:26 +0900

    change the url in post api

--- src/api/posts.js
+++ src/api/posts.js
@@ -3,12 +3,12 @@ import { posts } from './index';
 
 // 학습 노트 데이터를 조회하는 API
 function fetchPosts() {
-  return posts.get('posts');
+  return posts.get('/');
 }
 
 // 학습 노트 데이터를 생성하는 API
 function createPost(postData) {
-  return posts.post('posts', postData);
+  return posts.post('/', postData);
 }
 
 export { fetchPosts, createPost };
