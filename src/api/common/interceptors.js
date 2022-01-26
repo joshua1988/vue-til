dee24563f12c398a3f7ad9901e2e7e2ee5ba8a2c
goheeji <gmlwl0489@daum.net>
2022-01-26 16:30:42 +0900

    학습노트 조회 API 함수 구현

---
+++ src/api/common/interceptors.js
@@ -0,0 +1,33 @@
+import store from '@/store/index';
+
+export function setInterceptors(instance) {
+  // Add a request interceptor
+  instance.interceptors.request.use(
+    function(config) {
+      // Do something before request is sent
+      // console.log(config);
+      config.headers.Authorization = store.state.token;
+      console.log(config);
+      return config;
+    },
+    function(error) {
+      // Do something with request error
+      return Promise.reject(error);
+    },
+  );
+
+  // Add a response interceptor
+  instance.interceptors.response.use(
+    function(response) {
+      // Any status code that lie within the range of 2xx cause this function to trigger
+      // Do something with response data
+      return response;
+    },
+    function(error) {
+      // Any status codes that falls outside the range of 2xx cause this function to trigger
+      // Do something with response error
+      return Promise.reject(error);
+    },
+  );
+  return instance;
+}
