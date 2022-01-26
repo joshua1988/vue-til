7b6ffad8a5690801069a988f253d67e1a0d0fb03
goheeji <gmlwl0489@daum.net>
2022-01-26 17:12:13 +0900

    학습노트 데이터 조회 기능 개발

--- src/views/MainPage.vue
+++ src/views/MainPage.vue
@@ -2,27 +2,38 @@
   <div>
     <div class="main list-container contents">
       <h1 class="page-header">Today I Learned</h1>
-      <ul>
-        <li v-for="postItem in postItems" :key="postItem._id">
-          {{ postItems.title }}
-        </li>
+      <LoadingSpinner v-if="isLoading"></LoadingSpinner>
+      <ul v-else>
+        <PostListItem
+          v-for="postItem in postItems"
+          :key="postItem._id"
+          :postItem="postItem"
+        ></PostListItem>
       </ul>
     </div>
   </div>
 </template>
 
 <script>
+import PostListItem from '@/components/posts/PostListItem.vue';
+import LoadingSpinner from '@/components/common/LoadingSpinner.vue';
 import { fetchPosts } from '@/api/index';
 export default {
+  components: {
+    PostListItem,
+    LoadingSpinner,
+  },
   data() {
     return {
       postItems: [],
+      isLoading: false,
     };
   },
   methods: {
     async fetchData() {
+      this.isLoading = true;
       const { data } = await fetchPosts();
-      //   console.log(data.posts);
+      this.isLoading = false;
       this.postItems = data.posts;
     },
   },
